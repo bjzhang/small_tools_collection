746b8b933ae31122f97013469629fd1e2007a24e
Bamvor Jian Zhang <bamvor.zhangjian@linaro.org>
2016-10-21 20:24:23 +0800

    update
    Signed-off-by: Bamvor Jian Zhang <bamvor.zhangjian@linaro.org>

--- ssh_tools/ssh.py
+++ ssh_tools/ssh.py
@@ -4,10 +4,11 @@ import paramiko
 import time
 import logging
 import select
+import datetime
 
 def ssh_cmd(host, user, cmd, slient=True):
 #	print("Testing on " + host + " with user: " + user + " cmd: <" + cmd + ">")
-#	logging.getLogger("paramiko").setLevel(logging.WARNING)
+#	logging.getLogger("paramiko").setLevel(logging.WARNING) ssh = paramiko.SSHClient()
 	ssh = paramiko.SSHClient()
 	ssh.load_system_host_keys()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -53,9 +54,12 @@ def run_benchmark(host, normal_user, root_user, grubentry, total_count, test_use
 	print(grubentry)
 	print(total_count)
 	print(test_user)
+	dt = datetime.datetime.now()
+	print(str(dt))
 	for i, el in enumerate(testcmd):
 		print(el)
 
+	ssh_wait_connection(host, normal_user)
 	if reboot:
 		print("Changing grub.cfg")
 		ssh_cmd(host, root_user, ['sed -i "s/^set.default.*$/set default=' + grubentry + '/g" /boot/EFI/grub2/grub.cfg'], False)
@@ -79,13 +83,21 @@ def run_benchmark(host, normal_user, root_user, grubentry, total_count, test_use
 		ssh_transport(host, test_user, testcmd)
 		count = count + 1
 
-#run_benchmark(host="D03-02", normal_user="z00293696", root_user="root", grubentry="z00293696-upstream", total_count=5, test_user="z00293696", testcmd=["cd /home/z00293696/works/source/testsuite/lmbench/lmbench-3.0-a9; make rerun"])
-#
-#run_benchmark(host="D03-02", normal_user="z00293696", root_user="root", grubentry="z00293696-upstream-ilp32-disabled", total_count=5, test_user="z00293696", testcmd=["cd /home/z00293696/works/source/testsuite/lmbench/lmbench-3.0-a9; make rerun"])
+no_ilp32="z00293696-upstream-no-aarch32"
+ilp32_disabled="z00293696-upstream-ilp32-disabled-no-aarch32"
 
-run_benchmark(host="D03-02", normal_user="z00293696", root_user="root", grubentry="z00293696-upstream-ilp32-disabled", total_count=1, test_user="root", testcmd=["cd /home/z00293696/speccpu2006;. ./shrc; runspec --config=Arm64-single-core-linux64-arm64-lp64-gcc49.cfg --size=ref --noreportable --tune=base --iterations=1 perlbench bzip2 gcc mcf gobmk hmmer sjeng libquantum h264ref omnetpp astar xalancbmk"])
-#run_benchmark(host="D03-02", normal_user="z00293696", root_user="root", grubentry="z00293696-upstream-ilp32-disabled", total_count=1, test_user="root", testcmd=["cd /home/z00293696/speccpu2006;. ./shrc; runspec --config=Arm64-single-core-linux64-arm64-lp64-gcc49.cfg --size=ref --noreportable --tune=base --iterations=1 mcf hmmer"])
+round_max = 6
+round_cur = 0
 
-run_benchmark(host="D03-02", normal_user="z00293696", root_user="root", grubentry="z00293696-upstream", total_count=1, test_user="root", testcmd=["cd /home/z00293696/speccpu2006;. ./shrc; runspec --config=Arm64-single-core-linux64-arm64-lp64-gcc49.cfg --size=ref --noreportable --tune=base --iterations=1 perlbench bzip2 gcc mcf gobmk hmmer sjeng libquantum h264ref omnetpp astar xalancbmk"])
-#run_benchmark(host="D03-02", normal_user="z00293696", root_user="root", grubentry="z00293696-upstream", total_count=1, test_user="root", testcmd=["cd /home/z00293696/speccpu2006;. ./shrc; runspec --config=Arm64-single-core-linux64-arm64-lp64-gcc49.cfg --size=ref --noreportable --tune=base --iterations=1 mcf"], reboot=False)
+while (round_cur < round_max):
+	print("round: " + str(round_cur))
+	run_benchmark(host="D03-02", normal_user="z00293696", root_user="root", grubentry=no_ilp32, total_count=5, test_user="z00293696", testcmd=["cd /home/z00293696/works/source/testsuite/lmbench/lmbench-3.0-a9; make rerun"])
+	run_benchmark(host="D03-02", normal_user="z00293696", root_user="root", grubentry=ilp32_disabled, total_count=5, test_user="z00293696", testcmd=["cd /home/z00293696/works/source/testsuite/lmbench/lmbench-3.0-a9; make rerun"])
+
+	run_benchmark(host="D03-02", normal_user="z00293696", root_user="root", grubentry=ilp32_disabled, total_count=1, test_user="root", testcmd=["cd /home/z00293696/speccpu2006;. ./shrc; runspec --config=Arm64-single-core-linux64-arm64-lp64-gcc49.cfg --size=ref --noreportable --tune=base --iterations=1 perlbench bzip2 gcc mcf gobmk hmmer sjeng libquantum h264ref omnetpp astar xalancbmk"])
+	#run_benchmark(host="D03-02", normal_user="z00293696", root_user="root", grubentry=ilp32-disabled, total_count=1, test_user="root", testcmd=["cd /home/z00293696/speccpu2006;. ./shrc; runspec --config=Arm64-single-core-linux64-arm64-lp64-gcc49.cfg --size=ref --noreportable --tune=base --iterations=1 mcf hmmer"])
+
+	run_benchmark(host="D03-02", normal_user="z00293696", root_user="root", grubentry=no_ilp32, total_count=1, test_user="root", testcmd=["cd /home/z00293696/speccpu2006;. ./shrc; runspec --config=Arm64-single-core-linux64-arm64-lp64-gcc49.cfg --size=ref --noreportable --tune=base --iterations=1 perlbench bzip2 gcc mcf gobmk hmmer sjeng libquantum h264ref omnetpp astar xalancbmk"])
+	#run_benchmark(host="D03-02", normal_user="z00293696", root_user="root", grubentry=no_ilp32, total_count=1, test_user="root", testcmd=["cd /home/z00293696/speccpu2006;. ./shrc; runspec --config=Arm64-single-core-linux64-arm64-lp64-gcc49.cfg --size=ref --noreportable --tune=base --iterations=1 mcf"], reboot=False)
+	round_cur = round_cur + 1
 
