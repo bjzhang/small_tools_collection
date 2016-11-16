fb890effda30f4f6c684433c193b4addb67bc5b4
Bamvor Jian Zhang <bamvor.zhangjian@linaro.org>
2016-11-16 20:16:17 +0800

    add dryrun
    Signed-off-by: Bamvor Jian Zhang <bamvor.zhangjian@linaro.org>

--- ssh_tools/ssh.py
+++ ssh_tools/ssh.py
@@ -18,7 +18,16 @@ import os
 import sys	#for exit
 import subprocess
 import json	#for json.load
+import traceback#for print function name through extract_stack
+
 from pprint import pprint	#for pprint.pprint
+
+debug=False
+test_mode=False
+
+def function_name():
+	#ref <http://stackoverflow.com/questions/251464/how-to-get-a-function-name-as-a-string-in-python>
+	return traceback.extract_stack(None, 2)[0][2]
 
 def run_cmd_block(cmd):
 	subprocess.check_call(args = cmd)
@@ -134,9 +143,13 @@ def scp(host, user, src, dst, silent=True):
 	#TODO: chmod, chown
 	pbar.finish()
 
-def scp_s2s(src, src_user, dst, dst_user, src_path, dst_path):
+def scp_s2s(src, src_user, dst, dst_user, src_path, dst_path, dryrun=False):
+
 	cmd = ['scp', '-p', src_user + "@" + src + ":" + src_path, dst_user + "@" + dst + ":" + dst_path]
 	print(cmd)
+	if dryrun:
+		return
+
 	ssh_cmd(dst, dst_user, ['mkdir -p ' + os.path.dirname(dst_path)], slient=True)
 	run_cmd_block(cmd)
 
@@ -175,20 +188,22 @@ def run_benchmark(host, normal_user, root_user, grubentry, total_count, test_use
 	dt = datetime.datetime.now()
 	print("test finish at " + str(dt))
 
-def compile_kernel(host, user, path, commit, extra_config, dry_run=False, silent=False):
+def compile_kernel(host, user, path, commit, extra_config, dry_run=False, silent=False, checkout_force=False, dryrun=False):
 	dt = datetime.datetime.now()
 	print("compile_kernel start at " + str(dt))
 
-	ssh_wait_connection(host, user, dry_run)
+	if not dryrun:
+		ssh_wait_connection(host, user, dry_run)
 
-	cmd="cd " + path + "; git checkout -f " + commit
-	ssh_transport(host, user, [cmd], dry_run, silent=silent)
+		cmd="cd " + path + "; git checkout -f " + commit
+		ssh_transport(host, user, [cmd], dry_run, silent=silent)
 
-	cmd="export PATH=/home/z00293696/works/source/linux_toolkit/bin:/home/z00293696/works/software/ilp32-gcc/20160612_little_endian_toolchain/install/bin:$PATH; cd " + path + "; kernel_build --path " + path + " --disable install_header_to_cc"
-	for i, extra in enumerate(extra_config):
-		cmd = cmd + " --extra " + extra
+		cmd="export PATH=/home/z00293696/works/source/linux_toolkit/bin:/home/z00293696/works/software/ilp32-gcc/20160612_little_endian_toolchain/install/bin:$PATH; cd " + path + "; kernel_build --path " + path + " --disable install_header_to_cc"
+		for i, extra in enumerate(extra_config):
+			cmd = cmd + " --extra " + extra
 
-	ssh_transport(host, user, [cmd], dry_run, silent=silent)
+		ssh_transport(host, user, [cmd], dry_run, silent=silent)
+
 	print("compile_kernel end at " + str(dt))
 
 def get_files(folder):
@@ -204,7 +219,11 @@ def remote_copy_log_file(src, src_user, dst, dst_user, src_dir, dst_dir, files, 
 			print("scp -p " + src_user + "@" + src + ":" + src_dir + "/" + f + " " + dst_user + "@" + dst + ":" + dst_dir)
 		scp_s2s(src, src_user, dst, dst_user, p, dst_dir + "/" + f.rstrip('\n'))
 
-def run_benchmark_and_get_log(host, host_user, guest, guest_user, guest_root, this_grubentry, count, test_cmd, log_cmd, log_dir, host_log_dir, silent=False, reboot=True):
+def run_benchmark_and_get_log(host, host_user, guest, guest_user, guest_root, this_grubentry, count, test_cmd, log_cmd, log_dir, host_log_dir, silent=False, reboot=True, dryrun=False):
+	if dryrun:
+		print(function_name())
+		return
+
 	if log_cmd:
 		olds = ssh_cmd_get_log(host=guest, user=guest_user, cmd=log_cmd)
 
@@ -215,50 +234,87 @@ def run_benchmark_and_get_log(host, host_user, guest, guest_user, guest_root, th
 		print(new_logs)
 		remote_copy_log_file(guest, guest_user, host, host_user, log_dir, host_log_dir, new_logs)
 
-def run_test(config):
+def run_test(config, dryrun=False):
 	server = config["server"]
 	kernel = config["kernel"]
 	test = config["test"]
-	pprint(server)
-	pprint(kernel)
-	pprint(test)
+	if debug:
+		pprint(server)
+		pprint(kernel)
+		pprint(test)
+
 	for commit in kernel["commits"]:
-		log_path = server["log_base"] + "/" + commit["commit"] + "_" + commit["name"]
+		if server and "log_base" in server:
+			log_path = server["log_base"] + "/" + commit["commit"] + "_" + commit["name"]
 
-		image_path = kernel["path"] + "/arch/arm64/boot/Image"
-		config_path = kernel["path"] + "/.config"
-		compile_kernel(kernel["host"], kernel["user"], kernel["path"], commit["commit"], kernel["config_fragment"], silent=True)
-		scp_s2s(kernel["host"], kernel["user"], test["host"], test["root"], image_path, test["kernel_install"])
-		scp_s2s(kernel["host"], kernel["user"], server["host"], server["user"], image_path, log_path + "/" + commit["commit"] + "_" + commit["name"] + "_Image")
-		scp_s2s(kernel["host"], kernel["user"], server["host"], server["user"], config_path, log_path + "/" + commit["commit"] + "_" + commit["name"] + "_config")
+		#do not return if no kernel configure, we may run with the existing kernel in the machine.
+		if kernel and "path" in kernel:
+			image_path = kernel["path"] + "/arch/arm64/boot/Image"
+			config_path = kernel["path"] + "/.config"
+			compile_kernel(kernel["host"], kernel["user"], kernel["path"], commit["commit"], kernel["config_fragment"], silent=True, dryrun=dryrun)
+			if test and "kernel_install" in test:
+				scp_s2s(kernel["host"], kernel["user"], test["host"], test["root"], image_path, test["kernel_install"], dryrun=dryrun)
+				scp_s2s(kernel["host"], kernel["user"], server["host"], server["user"], image_path, log_path + "/" + commit["commit"] + "_" + commit["name"] + "_Image", dryrun=dryrun)
+				scp_s2s(kernel["host"], kernel["user"], server["host"], server["user"], config_path, log_path + "/" + commit["commit"] + "_" + commit["name"] + "_config", dryrun=dryrun)
+			else:
+				print("SKIP: install kernel and save kernel Image/config")
+		else:
+			print("skip compile kernel")
 
-		run_benchmark_and_get_log(server["host"], server["user"], test["host"], test["user"], test["root"], test["grub"], test["total_test_count"], test["testsuite"], test["log_cmd"], test["log_dir"], log_path)
+		if test and "testsuite" in test:
+			run_benchmark_and_get_log(server["host"], server["user"], test["host"], test["user"], test["root"], test["grub"], test["total_test_count"], test["testsuite"], test["log_cmd"], test["log_dir"], log_path, dryrun=dryrun)
+		else:
+			print("skip: run benchmark")
 
 def test():
 	host="localhost"
 	host_user="z00293696"
-	#ssh_transport(host, host_user, ["ls"])
-	#ssh_transport(host, host, ["sleep 1"])
+	ssh_transport(host, host_user, ["ls"])
+	ssh_transport(host, host_user, ["sleep 1"])
 
-	guest="d03-02"
-	guest_user="z00293696"
-	guest_root="root"
-	testsuite=["cd /home/z00293696/works; ls; screen -L ls source/kernel"]
-	run_benchmark_and_get_log(host, host_user, guest, guest_user, guest_root, "", 1, testsuite, "", "", "", reboot=False)
+	#guest="d03-09"
+	#guest_user="z00293696"
+	#guest_root="root"
+	#testsuite=["cd /home/z00293696/works; ls; screen -L ls source/kernel"]
+	#run_benchmark_and_get_log(host, host_user, guest, guest_user, guest_root, "", 1, testsuite, "", "", "", reboot=False)
 
-	testsuite=["echo sleep start; screen -L sleep 2; echo sleep done"]
-	run_benchmark_and_get_log(host, host_user, guest, guest_user, guest_root, "", 1, testsuite, "", "", "", reboot=False)
+	#testsuite=["echo sleep start; screen -L sleep 2; echo sleep done"]
+	#run_benchmark_and_get_log(host, host_user, guest, guest_user, guest_root, "", 1, testsuite, "", "", "", reboot=False)
+
+	print("FULL DRYRUN")
+	server = {u'host': u'bj23_new', u'log_base': u'/home/z00293696/works/source/testsuite/testresult/ilp32/20161104_specint_LP64_ilp32_on_aarch32_on', u'user': u'z00293696'}
+	kernel = {u'commits': [{u'commit': u'afb510f', u'name': u'ilp32_merged'}, {u'commit': u'a5ba168', u'name': u'ilp32_unmerged'}], u'config_fragment': [u'kernel_el0_config', u'kernel_ilp32_config', u'kernel_disable_arm_smmu_v3_config'], u'host': u'heyunlei', u'path': u'/home/z00293696/works/source/kernel/hulk', u'user': u'z00293696'}
+	test = {u'grub': u'z00293696-ilp32-test', u'host': u'D03-02', u'kernel_install': u'/boot/z00293696-ilp32-test', u'log_cmd': [u'cd /home/z00293696/speccpu2006/result; ls'], u'log_dir': u'/home/z00293696/speccpu2006/result', u'root': u'root', u'testsuite': [u'cd /home/z00293696/speccpu2006;. ./shrc; screen -L runspec --config=Arm64-single-core-linux64-arm64-lp64-gcc49.cfg --size=test,train,ref --noreportable --tune=base,peak --iterations=3 --verbose 0 bzip2 mcf hmmer libquantum'], u'total_test_count': 1, u'user': u'z00293696'}
+	config = {"server": server, "kernel": kernel, "test": test}
+	run_test(config, dryrun=True)
+
+	print("\nDRYRUN: skip kernel compile")
+	path=config["kernel"].pop("path")
+	run_test(config, dryrun=True)
+
+	print("\nDRYRUN: skip kernel install")
+	config["kernel"]["path"] = path
+	del config["test"]["kernel_install"]
+	run_test(config, dryrun=True)
+
+	print("\nDRYRUN: skip run benchmark")
+	del config["test"]["testsuite"]
+	run_test(config, dryrun=True)
+
 	sys.exit()
 
 print("START")
-#test()
-with open(sys.argv[1], 'r') as fp:
-	config = json.load(fp)
+if test_mode:
+	print("TEST")
+	test()
+else:
+	with open(sys.argv[1], 'r') as fp:
+		config = json.load(fp)
 
-run_test(config)
+	run_test(config)
 
-with open('config.json', 'w') as fp:
-	json.dump(config, fp)
+	with open('config.json', 'w') as fp:
+		json.dump(config, fp)
 
 print("END")
 
