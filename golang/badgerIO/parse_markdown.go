4183e91b8dfc3546f7e9cad808ff4cbb1660c014
Bamvor Zhang <bamv2005@gmail.com>
2018-02-24 16:48:59 +0800

    golang: parse first header of markdown. works but very ugly
    Signed-off-by: Bamvor Zhang <bamv2005@gmail.com>

---
+++ golang/badgerIO/parse_markdown.go
@@ -0,0 +1,85 @@
+
+// Reference <https://gobyexample.com/command-line-arguments> for how to get
+// arguments.
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "log"
+    "regexp"
+    "strings"
+)
+
+func parse_markdown(markdown string) {
+
+    file, err := os.Open(markdown)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    var prevLine string
+    var state string
+    state = "unknown"
+    var key string
+    var value string
+    for scanner.Scan() {
+        line := scanner.Text()
+        matched, err := regexp.MatchString("^=+", line)
+        if err != nil {
+            log.Fatal(err)
+        }
+        if (matched) {
+            fmt.Println("end get value. output previous key-value:")
+            value += prevLine + "\n"
+            fmt.Println("key: " + key)
+            fmt.Println("value: ")
+            fmt.Println(value)
+            key = ""
+            value = ""
+            fmt.Println("Found Key")
+            key = prevLine
+            state = "getValue"
+        } else {
+            if strings.Compare(state, "getValue") == 0 {
+                matched, err := regexp.MatchString("^=+", prevLine)
+                if err != nil {
+                    log.Fatal(err)
+                }
+                if !matched {
+                    value += prevLine + "\n"
+                }
+            }
+        }
+        prevLine = line
+    }
+    fmt.Println("EOF. output previous key-value:")
+    value += prevLine + "\n"
+    fmt.Println("key: " + key)
+    fmt.Println("value: " + value)
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+}
+
+func main() {
+    if (len(os.Args) < 3) {
+        log.Fatal("Usage: " + os.Args[0] + " /path/to/markdown /path/to/badgerIO")
+    }
+    markdown := os.Args[1]
+    if _, err := os.Stat(markdown); os.IsNotExist(err) {
+        log.Fatal("file<" + markdown + "> is not exist")
+    }
+    fi, err := os.Lstat(markdown)
+    if err != nil {
+        log.Fatal(err)
+    }
+    if (!fi.Mode().IsRegular()) {
+        fmt.Println("file<" + markdown + "> is not regular file")
+    }
+    parse_markdown(markdown)
+}
+
