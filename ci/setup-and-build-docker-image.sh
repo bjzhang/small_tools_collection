2fb8e38369126c2b24be738d1b1067ef6442be6b
Bamvor Zhang <bamv2005@gmail.com>
2018-07-18 20:51:58 +0800

    drone: update docker build image
    Signed-off-by: Bamvor Zhang <bamv2005@gmail.com>

--- ci/setup-and-build-docker-image.sh
+++ ci/setup-and-build-docker-image.sh
@@ -1,20 +1,20 @@
 # System update
-echo y | sudo apt-get remove docker docker-engine docker.io
-echo y | sudo apt-get update
+sudo apt-get remove -y docker docker-engine docker.io
+sudo apt-get update -y
 
 # Install docker
-echo y | sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-echo y | sudo apt-get update
-echo y | sudo apt-get install docker-ce
+sudo apt-get update -y
+sudo apt-get install -y docker-ce
 
 # Install qemu-user-static
-echo y | sudo apt-get install qemu-user-static
+sudo apt-get install -y qemu-user-static
 sudo docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
 # Install docker-compse
-echo y | sudo apt-get install docker-compose
+sudo apt-get install -y docker-compose
 
 # Install go
 wget https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
@@ -24,6 +24,7 @@ sudo mkdir /usr/local/go
 rm -rf go
 
 # Download source code
+echo "TODO switch to our repo. the Dockerfile in official repo need to patched"
 go get -d -u github.com/ipfs/go-ipfs
 
 echo Build docker images
