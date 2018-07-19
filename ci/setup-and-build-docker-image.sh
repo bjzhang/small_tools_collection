9467260a18f48d85c52a9780a1a4fa185e961ea3
Bamvor Zhang <bamv2005@gmail.com>
2018-07-19 15:44:55 +0800

    ci: switch to bitbucket
    Signed-off-by: Bamvor Zhang <bamv2005@gmail.com>

--- ci/setup-and-build-docker-image.sh
+++ ci/setup-and-build-docker-image.sh
@@ -5,10 +5,12 @@ sudo docker run --rm --privileged multiarch/qemu-user-static:register --reset
 
 # Download source code
 echo "TODO switch to our repo. the Dockerfile in official repo need to patched"
-go get -d -u github.com/ipfs/go-ipfs
+git config --global credential.helper store
+echo "https://BamvorZhang:gRt6UhC4WjnCjRmKThsv@bitbucket.org" >> ~/.git-credentials
+go get -d -u -v bitbucket.org/ipfsbit/go-ipfs
 
 echo Build docker images
-cd go/src/github.com/ipfs/go-ipfs
+cd ~/go/src/bitbucket.org/ipfsbit/go-ipfs
 cp -p /usr/bin/qemu-arm-static .
 sudo docker build . --file Dockerfile.armv7 -t bamvor/go-ipfs-armv7:latest
 
